8259c4e88667949c842d27a6b514e983bc40f765
Gabriel Castro <gabrielsilvadecastro777@gmail.com>
2023-10-19 21:54:02 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplica(valor1, valor2) {
+    return valor1 + valor2;
+}
+console.log(multiplica(10, 10));
+function saudação(nome) {
+    return `Olá ${nome}`;
+}
+console.log(saudação('Gabriel'));
---
+++ main.ts
@@ -0,0 +1,11 @@
+function multiplica(valor1: number, valor2: number): number {
+    return valor1 + valor2;
+}
+
+console.log(multiplica(10, 10));
+
+function saudação(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+console.log(saudação('Gabriel'));
